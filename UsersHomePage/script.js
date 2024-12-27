fe4e95a7443bc2753d66aca2bf3946d775d75f3d
jhncp <pablo.jhanice@gmail.com>
2024-12-27 18:52:43 +0800

    Updated

---
+++ UsersHomePage/script.js
@@ -0,0 +1,9 @@
+function openOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'flex'; // Show overlay
+}
+
+function closeOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none'; 
+}
