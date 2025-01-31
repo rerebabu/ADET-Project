99726f09afc035762b6a1e1983cb74308c548b86
Yhuwi <iygdomingo14@gmail.com>
2025-02-01 03:19:08 +0800

    css file for report details page

---
+++ adminPages/adminReportDetails.js
@@ -0,0 +1,10 @@
+document.querySelector('.connectBtn').addEventListener('click', function () {
+  alert('Connecting to the user...');
+  });
+
+function openPhoto() {
+  const photoUrl = '/path/to/photo.jpg'; 
+  window.open(photoUrl, '_blank'); 
+  }
+
+document.querySelector('.photoBtn').addEventListener('click', openPhoto);
